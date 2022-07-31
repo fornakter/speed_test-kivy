a2bb99c70fb35a6ee1716f7835d506bfd4e749a6
AdamF <fornakter@gmail.com>
2022-07-31 16:50:08 +0200

    App ready. Enjoy!

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
+import speedtest
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 
 
 class MainWidget(BoxLayout):
     status_label = StringProperty("Status: waiting")
+    server_label = StringProperty("Server:")
+    download_label = StringProperty('Download speed:')
+    upload_label = StringProperty('Upload speed:')
+    test = speedtest.Speedtest()
 
-    def on_button_click(self):
-        self.status_label = "Status: klick"
+    def on_press(self):
+        self.status_label = "Status: Loading server list..."
+
+    def on_release(self):
+        self.status_label = "Status: Speed testing..."
+        # Get list of servers
         try:
-            #import speedtest
-            from speedtest import Speedtest
+            self.test.get_servers()
         except Exception as e:
-            print('Error import: ', e)
-        try:
-            print("Loading server list...")
-            test = Speedtest()
-            # Get list of servers
-            test.get_servers()
-            # Choose best server
-            best = test.get_best_server()
-            # print(f"Found best server in: {best['country']}, {best['name']}")
-            print("Download test...")
-            download_result = test.download()
-            print("Upload test...")
-            # upload_result = test.upload()
-            print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
-            # print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
-        except Exception as e:
-            print('Another error: ', e)
+            print(e)
+        # Choose best server
+        best = self.test.get_best_server()
+        self.server_label = f"Found best server in: {best['country']}, {best['name']}"
+
+    def download_test(self):
+        download_result = self.test.download()
+        self.download_label = f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s"
+
+    def upload_test(self):
+        upload_result = self.test.upload()
+        self.status_label = "Status: Test complete"
+        self.upload_label = f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s"
+
 
 class MainMenu(App):
     pass
