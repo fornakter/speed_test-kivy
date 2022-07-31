e0d698e31c39e9fc137caa187a4a103582c32faf
AdamF <fornakter@gmail.com>
2022-07-31 14:48:52 +0200

    I cut everything what i can Error: 'LogFile' object has no attribute 'fileno'

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
-from only_test import speed_test
+
 
 class MainWidget(BoxLayout):
     status_label = StringProperty("Status: waiting")
@@ -9,7 +9,25 @@ class MainWidget(BoxLayout):
     def on_button_click(self):
         self.status_label = "Status: klick"
         try:
-            speed_test()
+            # Get list of servers
+            try:
+                import speedtest
+
+                print("Loading server list...")
+                test = speedtest.Speedtest()
+                test.get_servers()
+                # Choose best server
+                best = test.get_best_server()
+                # print(f"Found best server in: {best['country']}, {best['name']}")
+                print("Download test...")
+                download_result = test.download()
+                print("Upload test...")
+                # upload_result = test.upload()
+                print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
+                # print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
+            except Exception as e:
+                print(e)
+
         except Exception as e:
             print(e)
 
