8303ecae6680029704a7ffd0a068a76335e69099
AdamF <fornakter@gmail.com>
2022-07-31 15:07:04 +0200

    I cut everything what i can Error: 'LogFile' object has no attribute 'fileno'

--- main.py
+++ main.py
@@ -9,28 +9,26 @@ class MainWidget(BoxLayout):
     def on_button_click(self):
         self.status_label = "Status: klick"
         try:
-            # Get list of servers
-            try:
-                import speedtest
-
-                print("Loading server list...")
-                test = speedtest.Speedtest()
-                test.get_servers()
-                # Choose best server
-                best = test.get_best_server()
-                # print(f"Found best server in: {best['country']}, {best['name']}")
-                print("Download test...")
-                download_result = test.download()
-                print("Upload test...")
-                # upload_result = test.upload()
-                print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
-                # print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
-            except Exception as e:
-                print(e)
-
+            #import speedtest
+            from speedtest import Speedtest
         except Exception as e:
-            print(e)
-
+            print('Error import: ', e)
+        try:
+            print("Loading server list...")
+            test = Speedtest()
+            # Get list of servers
+            test.get_servers()
+            # Choose best server
+            best = test.get_best_server()
+            # print(f"Found best server in: {best['country']}, {best['name']}")
+            print("Download test...")
+            download_result = test.download()
+            print("Upload test...")
+            # upload_result = test.upload()
+            print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
+            # print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
+        except Exception as e:
+            print('Another error: ', e)
 
 class MainMenu(App):
     pass
